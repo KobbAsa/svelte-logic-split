d0be3b1212fe0b9d27ec846d28ac64d163b14e5e
Antonio <antony.dovzhenko@gmail.com>
2025-06-19 14:19:02 +0300

    Add business logic in service layer

---
+++ src/services/countryService.js
@@ -0,0 +1,44 @@
+// Business logic
+
+import { countryStore } from '../stores/countryStore.js';
+import * as countriesApi from '../api/countriesApi.js';
+
+export async function findCountry(name) {
+    if (!name || name.trim() === '') {
+        countryStore.set({
+            country: null,
+            isLoading: false,
+            error: 'Please enter country name.',
+        });
+        return;
+    }
+
+    countryStore.update(store => ({
+        ...store,
+        isLoading: true,
+        error: null,
+        country: null,
+    }));
+
+    try {
+        const countries = await countriesApi.searchCountryByName(name.trim());
+
+        const foundCountry = countries[0];
+
+        countryStore.update(store => ({
+            ...store,
+            country: foundCountry,
+        }));
+
+    } catch (err) {
+        console.error('Service: Caught error ->', err.message);
+        countryStore.update(store => ({
+            ...store,
+            country: null,
+            error: err.message,
+        }));
+
+    } finally {
+        countryStore.update(store => ({ ...store, isLoading: false }));
+    }
+}
